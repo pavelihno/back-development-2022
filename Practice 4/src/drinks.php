34a8d84b2907ef4bca8115d08fa8c9e858744bf0
pavelihno <crazzyyellow02@gmail.com>
2022-10-25 11:53:22 +0300

    css перенесены в static

--- Practice 4/src/drinks.php
+++ Practice 4/src/drinks.php
@@ -1,11 +1,12 @@
 <html lang="en">
 <head>
 <title>Drinks</title>
-    <link rel="stylesheet" href="style.css" type="text/css"/>
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-<h1>Drinks</h1>
-<table>
+<h1 class="display-4">Drinks</h1>
+<table class="table table-striped">
     <tr><th></th><th>Price</th></tr>
 
 <?php
