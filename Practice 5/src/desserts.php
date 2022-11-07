b77b9860dcd67c968a858763b1e2d5f98859adad
pavelihno <crazzyyellow02@gmail.com>
2022-11-07 11:35:59 +0300

    создана основа practice 5

---
+++ Practice 5/src/desserts.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+<title>Desserts</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<h1 class="display-4">Desserts</h1>
+<table class="table table-striped">
+    <tr><th></th><th>Price</th><th>Calories</th></tr>
+
+<?php
+try
+{
+    $sql_connection = mysqli_connect("db", "root", "12345678", "appDB");
+    $result = $sql_connection->query("SELECT * FROM desserts");
+
+    foreach ($result as $row)
+    {
+        echo "<tr><td>{$row['title']}</td><td>{$row['price']}rub </td><td>{$row['calories']}</td></tr>";
+    }
+}
+catch(mysqli_sql_exception $e)
+{
+    print ("Ошибка подключения к БД");
+}
+?>
+
+</table>
+
+</body>
+</html>
